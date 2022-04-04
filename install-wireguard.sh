c82279848a2a6a901d8fcfff25008a66fd59fb89
Elchibek <timele96@gmail.com>
2022-04-04 14:16:55 +0300

    Create install-wireguard.sh

---
+++ install-wireguard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
+
+function isRoot() {
+	if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+	fi
+}
+
+function installWireGuard() {
+	# Install WireGuard tools and module
+	apt-get update
+	apt-get install -y wireguard iptables
+	# Убедитесь, что каталог существует (это не относится к Fedora)
+	chmod 600 -R /etc/wireguard/
+
+	# Включить маршрутизацию на сервере
+	echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+
+	systemctl start "wg-quick@${SERVER_WG_NIC}"
+	systemctl enable "wg-quick@${SERVER_WG_NIC}"
+}
+
+# Check for root, virt, OS...
+isRoot
+installWireGuard
